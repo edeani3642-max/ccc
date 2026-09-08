c5302fe11fa6306d2ff2257b2047726d793c5471
dydx <edeani364.2@gmail.com>
2026-09-08 04:20:26 +0100

    Changed the hero catchphrase

---
+++ app/debug/page.tsx
@@ -0,0 +1,10 @@
+import CodeBlock from "@/components/editor/CodeBlock";
+import { language } from "@codemirror/language";
+
+export default function DebugPage(){
+    return(
+        <>
+            <CodeBlock value="" language="python"/>
+        </>
+    )
+}
--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -123,7 +123,7 @@ export default function Hero() {
                         </p>
 
                         <h1 className="mx-auto max-w-4xl text-[1.2rem] font-semibold leading-[1.05] tracking-tight sm:text-5xl sm:leading-none md:text-6xl lg:text-7xl">
-                            Learning programming restructured to meet
+                            Study of programming restructured to meet
                             current needs through interactive learning
                         </h1>
 
